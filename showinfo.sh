53a46598f302e92eb32a8d4d497f7526590ffaa8
yu.qin <yu.qin@ck-telecom.com>
2017-08-15 14:01:42 +0800

    create showinfo.sh to display name and age

---
+++ showinfo.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# desc		this is to show your nage and age
+# author 	pecuyu
+# date 		2017.8.15
+# version	1.0
+
+read -p "please enter your name:" name 
+read -p "please enter your age:" age
+echo "your name is ${name},your age is ${age}"
+
+exit 0
+
