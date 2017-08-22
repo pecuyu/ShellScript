3d4cf908f72234f12c2ea3e144d525282422415d
yu.qin <yu.qin@ck-telecom.com>
2017-08-22 12:38:23 +0800

    add positino argumetns sh files

---
+++ shell_pos_args.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# desc		this shell script is to show position args 
+# author 	pecuyu
+# date 		2017.8.15
+# version	1.0
+
+# do something here 
+
+echo "the program's name is ${0}"
+echo "the args number is : $# "
+if [ "$#" -lt "2" ]; 
+	then echo "the number of args is less than 2 " ;  exit 0
+fi
+
+echo "the whole input args is : $@"
+echo "the first agrs is : ${1}"
+echo "the second agrs is : ${2}"
+
+#end 
